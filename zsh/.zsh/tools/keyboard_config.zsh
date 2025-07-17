0132221c8ed4d432d94eceb6b9f7009b8ada1822
Omar Espana <omi.fullstack@gmail.com>
2025-07-17 17:27:55 +0100

    Adding nvim submodule and cleaning keyboard configs

--- zsh/.zsh/tools/keyboard_config.zsh
+++ zsh/.zsh/tools/keyboard_config.zsh
@@ -1,11 +1,8 @@
-function set_ext_keyboard ()
-{  
-  local picked_conf=$(gum choose $(ls $EXT_KEYBOARD_CONFIGS | grep "yaml"))
-  local keyboard_configuration=$EXT_KEYBOARD_CONFIGS/$picked_conf 
-  eval "$EXT_KEYBOARD_TOOL validate < $keyboard_configuration" &&
-  eval "$EXT_KEYBOARD_TOOL upload < $keyboard_configuration"
-  echo "Setted $picked_conf"
-  print_layout $keyboard_configuration
+function set_ext_keyboard() {
+    local picked_conf=$(gum choose $(ls "$EXT_KEYBOARD_CONFIGS" | grep "yaml"))
+    local keyboard_configuration=$EXT_KEYBOARD_CONFIGS/$picked_conf
+    eval "$EXT_KEYBOARD_TOOL validate < $keyboard_configuration" &&
+        eval "$EXT_KEYBOARD_TOOL upload < $keyboard_configuration"
+    echo "Setted $picked_conf"
+    print_layout "$keyboard_configuration"
 }
-
-
